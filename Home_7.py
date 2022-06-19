8521df24d5fa346d0c141b46dd4a404976931037
legion1983 <legion1983@narod.ru>
2022-06-20 00:05:25 +0300

    Initial commit

--- Home_7.py
+++ Home_7.py
@@ -1,2 +1,26 @@
-Print('Hello World eewrewrewrwerwerew')
-Print('1111')
+# Программа для кулинарной книги
+# осталось только придумать как строку превратить в словарь
+# sort в 3ей задаче
+
+from pprint import pprint
+file_name = "list _of_dishes.txt"
+def cooking_book (file_name):
+    with open(file_name, 'r', encoding="utf-8") as file:
+        result = {}
+        for line in file:
+            dish_name = line.strip()
+            ingredients = []
+            for item in range(int(file.readline())):
+                ingredient = file.readline().strip()
+                ingredient = ingredient.split("|")
+                ingredient = {'ingredient_name': ingredient[0], "quantaty": ingredient[1], "measure": ingredient[2]}
+                ingredients.append(ingredient)
+            result[dish_name] = ingredients
+            # фейковое чтение чтобы убрать строку пустую file.readline()
+            file.readline()
+        return result
+
+book = cooking_book(file_name)
+pprint(book)
+
+
