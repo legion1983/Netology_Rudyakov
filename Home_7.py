7264234f4f55ab788d318b14da1711512dd1eeda
legion1983 <legion1983@narod.ru>
2022-06-20 23:30:50 +0300

    Initial commit2222

--- Home_7.py
+++ Home_7.py
@@ -1,7 +1,4 @@
-# Программа для кулинарной книги
-# осталось только придумать как строку превратить в словарь
-# sort в 3ей задаче
-
+# --------------------------------------Task_1_creation cook_book----------------------------------------
 from pprint import pprint
 file_name = "list _of_dishes.txt"
 def cooking_book (file_name):
@@ -21,6 +18,19 @@ def cooking_book (file_name):
         return result
 
 book = cooking_book(file_name)
-pprint(book)
 
+# -----------------------------------------------Task_2--------------------------------------------
+def get_shop_list_by_dishes(dishes, person_count):
+    dict_products_for_purchusing = dict()
+    for values in book.keys():
+        if values in dishes:
+            print(values)
+            q_ty_of_ingridients = len(book[values])
+
+            for items in range(q_ty_of_ingridients):
+                weight_of_ingridients = {'measure': book[values][items]['measure'], 'quantity': person_count*int(book[values][items]["quantaty"])}
+                product_name = (book[values][items]['ingredient_name'])
+                dict_products_for_purchusing[product_name] = weight_of_ingridients
+    pprint(dict_products_for_purchusing)
+get_shop_list_by_dishes(['Утка по-пекински', 'Омлет'], 2)
 
