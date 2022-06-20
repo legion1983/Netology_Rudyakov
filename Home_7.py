513676e0e2a14769135fbe6149c7b37305911fcb
legion1983 <legion1983@narod.ru>
2022-06-21 00:19:32 +0300

    Initial commit2222

--- Home_7.py
+++ Home_7.py
@@ -1,3 +1,5 @@
+# -------------------Домашнее задание к лекции «Открытие и чтение файла, запись в файл»-----------------------
+
 # --------------------------------------Task_1_creation cook_book----------------------------------------
 from pprint import pprint
 file_name = "list _of_dishes.txt"
@@ -18,19 +20,60 @@ def cooking_book (file_name):
         return result
 
 book = cooking_book(file_name)
-
+print('')
+print('------------------------Result for task 7_1------------------------------------------------')
+print('')
+pprint(book)
+print('')
 # -----------------------------------------------Task_2--------------------------------------------
 def get_shop_list_by_dishes(dishes, person_count):
     dict_products_for_purchusing = dict()
     for values in book.keys():
         if values in dishes:
-            print(values)
+
             q_ty_of_ingridients = len(book[values])
 
             for items in range(q_ty_of_ingridients):
                 weight_of_ingridients = {'measure': book[values][items]['measure'], 'quantity': person_count*int(book[values][items]["quantaty"])}
                 product_name = (book[values][items]['ingredient_name'])
                 dict_products_for_purchusing[product_name] = weight_of_ingridients
+    print('')
+    print('------------------------Result for task 7_2------------------------------------------------')
+    print('')
     pprint(dict_products_for_purchusing)
 get_shop_list_by_dishes(['Утка по-пекински', 'Омлет'], 2)
 
+
+
+# -----------------------------------------------Task_3--------------------------------------------
+file_name_1 = '7_1.txt'
+file_name_2 = '7_2.txt'
+file_name_3 = '7_3.txt'
+
+def reading_files(file_name):
+    with open(file_name, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
+        number_of_string = 0
+        for line in lines:
+            number_of_string += 1
+    return(number_of_string)
+reading_files(file_name_1)
+reading_files(file_name_2)
+reading_files(file_name_3)
+
+dict = {file_name_1: reading_files(file_name_1), file_name_2: reading_files(file_name_2), file_name_3: reading_files(file_name_3)}
+dict = {k: dict[k] for k in sorted(dict, key=dict.get, reverse=False)}
+
+print('')
+print('------------------------Result for task 7_3------------------------------------------------')
+print(dict)
+
+
+# ------Example N1 ------
+# def logger(file_name, mode, data):
+#     with open(file_name, 'a') as file_obj:
+#         file_obj.write(f"{data}\n")
+# # \n при записи в файл, мы сами должны контролировать переход каретки на следующую строку с помощью \n
+# logger(file_name, 'data')
+# logger(file_name, 'data')
+# logger(file_name,  'data')
