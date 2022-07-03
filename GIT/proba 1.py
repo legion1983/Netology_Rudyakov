b97d4c428aba4963e6a94eb8873e4e19200f5c97
Pavel Rudyakov <legion1983@narod.ru>
2022-07-03 17:30:19 +0300

    Git task_!

---
+++ GIT/proba 1.py
@@ -0,0 +1,14 @@
+git branch - ветку создавать
+git checkout .... путь к ветке ...  - переключение на другую ветку
+git checkout master - переход на ветку мастер
+git log - посмотреть истрорию изменений
+git remoute add origin .. ccылка ан GIT HUB ... - забросит файлы на GIT HUB
+git push -u origin  ....путь к ветке...
+git commit -a -m
+git config --list --show-origin - посмотреть все настройки
+git config --global user.name "Pavel Rudyakov" - поменять имя пользователя
+git config - global user.email legion1983@narod.ru -поменять почту пользователя
+git config --global core.editor emacs - использовать другой редактор для редактирования и добавления файлов в git
+git init - инициализация репозитория
+git status - статус фалов в гит
+git help - помощь
